823c7560f87a4aa236550cd13ee0455d89471ee2
yabouseada <y.abouseada@gmail.com>
2021-09-25 17:39:14 +0200

    added postgressql

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
@@ -10,8 +11,12 @@ from resources.item import Item,ItemList
 from resources.store import Store,StoreList
 from db import db
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get(uri,'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 app.secret_key = 'yahia'
 api= Api(app)
